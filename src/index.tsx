e8fa096b44ebea12c6325c0aead949ce5c148de8
jai-source <jairatna54@gmail.com>
2025-11-13 00:55:38 +0530

    Revert "login/reg page + logo addition + misc"

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Desktop } from "./Desktop";
-import { Registration } from "./Registration";
-import { Login } from "./Login";
-import { AuthChoice } from "./AuthChoice";
+import App from "./App";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Desktop />} />
-        <Route path="/auth-choice" element={<AuthChoice />} />
-        <Route path="/register" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
